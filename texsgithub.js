b6e5d8cea90f8cc929ffbc8732999e52d3feca8a
Bob <bob@email.com>
2021-09-06 17:27:45 +0700

    test

--- texsgithub.js
+++ texsgithub.js
@@ -1,12 +1,3 @@
-import React from 'react';
-import { View } from 'react-native';
-
-const componentName = ({
-    children, style, ...rest
-}) => (
-    <View style={style}>
-        {children}
-    </View>
-);
-
-export default componentName;
+const styles = StyleSheet.create({
+    
+})
