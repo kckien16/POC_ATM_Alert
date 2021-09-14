13dd4a48a52dddee3de1ce9e4654deed1bf72789
VanDaiN <nguyenvandai30062001@gmail.com>
2021-09-14 12:59:33 +0700

    test

--- constants/Fonts.tsx
+++ constants/Fonts.tsx
@@ -3,7 +3,7 @@ export default {
     h2:{fontSize: 16, fontWeight: '700', fontFamily:"Mulish", fontStyle:"normal",color: "#091F3A",lineHeight:20},
     h3:{fontSize: 16, fontWeight: '400', fontFamily:"Mulish", fontStyle:"normal",color: "#091F3A",lineHeight:24},
     h4:{fontSize: 14, fontWeight: '700', fontFamily:"Mulish", fontStyle:"normal",color: "#091F3A",lineHeight:20},
-    h5:{fontSize: 14, fontWeight: '400', fontFamily:"Mulish", fontStyle:"normal",color: "#2190CD",lineHeight:20},
+    h5:{fontSize: 12, fontWeight: '400', fontFamily:"Mulish", fontStyle:"normal",color: "#1085C5",lineHeight:20},
     h6:{fontSize: 14, fontWeight: '400', fontFamily:"Mulish", fontStyle:"normal",color: "#091F3A",lineHeight:20},
     h7:{fontSize: 16, fontWeight: '800', fontFamily:"Mulish", fontStyle:"normal",color: "#2190CD",lineHeight:20},
 }
